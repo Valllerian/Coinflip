d89c87730309869c0a438fa8112eabb37aea8f71
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-28 19:43:58 -0600

    gif added

--- src/Flipper/Flipper.jsx
+++ src/Flipper/Flipper.jsx
@@ -3,6 +3,7 @@ import Coin from "../Coin/Coin";
 
 import Heads from "./heads.jpeg"; // with import
 import Tails from "./tails.jpeg"; // with import
+import Gif from "./eatingCerbe.gif"; // with import
 
 const Flipper = () => {
   const [coin, setCoin] = useState(Heads);
@@ -23,13 +24,14 @@ const Flipper = () => {
     }
   };
   return (
-    <div className="shadow-xl border-2 border-[#f472b6] bg-white p-5 content-end rounded-md ">
+    <div className="shadow-xl border-2 border-black bg-white p-5 content-end rounded-md ">
       <div className="mb-4 font-serif">Let`s flip a Cerbe-Coin!</div>
       <Coin coin={coin} />
       <button className="ease inline-block cursor-pointer rounded-full bg-[#fbcfe8] px-4 py-1 mt-8 mb-4 text-xl text-black transition duration-500 hover:bg-gray-200 shadow-xl  border-2 border-black font-serif" onClick={(e) => flipCoin(e)}>Flip it!</button>
       <div className="font-serif">
         Out of {flips} flips, there have been {heads} Cerbe-Heads and {tails} Cerbe-Tails.
       </div>
+      <div className="absolute bottom-0 left-0"><img src={Gif} alt="" /></div>
     </div>
   );
 };
